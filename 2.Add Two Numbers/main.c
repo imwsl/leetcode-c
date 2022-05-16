d34a7297a67472bc77249bfc258b42ee6a99a9d5
wesley <wesley.yang789@hotmail.com>
2022-05-16 13:23:02 +0800

    add reverse_list functions

--- 2.Add Two Numbers/main.c
+++ 2.Add Two Numbers/main.c
@@ -19,6 +19,15 @@ int length_of_list(struct ListNode* l) {
     return length;
 }
 
+void puts_list(struct ListNode* l) {
+    
+    struct ListNode* p = l;
+    while (p != NULL) {
+        printf("%d\n", p->val);
+        p = p->next;
+    }
+}
+
 struct ListNode* create_list(int* arr, int size) {
     struct ListNode* head = NULL;
     struct ListNode* p = NULL;
@@ -41,16 +50,77 @@ struct ListNode* create_list(int* arr, int size) {
     return head;
 }
 
-struct ListNode* addTwoNumber(struct ListNode* l1, struct ListNode* l2) {
+struct ListNode* reverse_list(struct ListNode* l) {
+    return NULL;
+}
 
+struct ListNode* addTwoNumber(struct ListNode* l1, struct ListNode* l2) {
+    int len_of_l1 = length_of_list(l1);
+    int len_of_l2 = length_of_list(l2);
+    int min_len = len_of_l1 < len_of_l2 ? len_of_l1 : len_of_l2;
+
+    int i = 0;
+    int is_upped = 0;
+
+    struct ListNode* p1 = l1;
+    struct ListNode* p2 = l2;
+    struct ListNode* p3 = NULL;
+    struct ListNode* prev_p = NULL;
+
+    struct ListNode* head = (struct ListNode *)malloc(sizeof(struct ListNode));
+    struct ListNode* p = head;
+    p->val = 0;
+    p->next = NULL;
+
+    for (i = 0; i < min_len; i ++) {
+        int temp = (p1->val + p2->val + is_upped) % 10;
+        is_upped = (p1->val + p2->val + is_upped) / 10;
+
+        p->val = temp;
+        p->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+        prev_p = p;
+        p = p->next;
+        p->next = NULL;
+
+        p1 = p1->next;
+        p2 = p2->next;
+    }
+
+    p3 = (len_of_l1 == len_of_l2) ? NULL : ((len_of_l1 > len_of_l2) ? p1 : p2);
+    while(NULL != p3) {
+        int temp = (p3->val + is_upped) % 10;
+        is_upped = (p3->val + is_upped) / 10;
+
+        p->val = temp;
+        p->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+        prev_p = p;
+        p = p->next;
+        p->next = NULL;
+
+        p3 = p3->next;
+    }
+
+    if (is_upped == 1) {
+        p->val = is_upped;
+    } else {
+        if (NULL != prev_p->next) {
+            free(prev_p->next);
+        }
+        prev_p->next = NULL;
+    }
+
+    return head;
 }
 
 int main() {
     
-    int arr[5] = {1, 2, 3, 4, 5};
-    struct ListNode * l1 = create_list(arr, 5);
-    int len = length_of_list(l1);
-    printf("%d\n", len);
+    int arr1[5] = {1, 2, 3, 4, 5};
+    int arr2[] = {1, 2, 9};
+    struct ListNode* l1 = create_list(arr1, 5);
+    struct ListNode* l2 = create_list(arr2, 3);
+
+    struct ListNode* head = addTwoNumber(l1, l2);
+    puts_list(head);
 
     return 0;
 }
