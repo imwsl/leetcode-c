0bd62ed9b601ed1f5633ee7f738b73f7a23750e8
wesley <wesley.yang789@hotmail.com>
2022-06-01 19:37:14 +0800

    add files

--- 3. Longest Substring Without Repeating Characters/main.c
+++ 3. Longest Substring Without Repeating Characters/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Cell {
   char ch;
@@ -22,31 +23,95 @@ int is_char_in_cell_list(struct Cell* l, char ch) {
 
   while(NULL != p) {
     if (p->ch == ch) return 1;
+    p = p->next;
   }
 
   return 0;
 }
 
-int lengthOfLongestSubstring(char* ch) {
-  int len = strlen(ch);
+void free_cell_list(struct Cell* l) {
+  struct Cell *p = l;
+  struct Cell *curr_p = l;
+  
+  while(NULL != p) {
+    
+    p = p->next;
+    free(curr_p);
+    curr_p = p;
+  }
+}
+
+int lengthOfLongestSubstring(char* s) {
+  int len = strlen(s);
 
   int max_len = 0;
   struct Cell *head = NULL;
   struct Cell *p = NULL;
   
-  if (len <= 2) return len;
-  
-  for (int i = 0; i < len; i ++) {
-    
-    for (int j = i + 1; j < len; j ++) {
-      
+  if (len == 0) return 0;
+  if (len == 1) return 1;
+  if (len == 2) {
+    if (s[0] == s[1]) return 1;
+    else {
+      return 2;
     }
   }
   
-  return 0;
+  for (int i = 0; i < len; i ++) {
+    
+    if (NULL == head) {
+      head = (struct Cell*)malloc(sizeof(struct Cell));
+      head->ch = s[i];
+      head->next = NULL;
+      p = head;
+    }
+
+    for (int j = i + 1; j < len; j ++) {
+      
+      if (0 == is_char_in_cell_list(head, s[j])) {
+        
+        struct Cell* temp = (struct Cell*)malloc(sizeof(struct Cell));
+        temp->ch = s[j];
+        temp->next = NULL;
+        p->next = temp;
+        p = temp;
+      } else {
+        break;
+      } 
+    }
+
+    if (NULL != head) {
+
+      int temp_len = length_of_cell_list(head);
+      max_len = (max_len > temp_len) ? max_len : temp_len;
+
+      free_cell_list(head);
+      head = NULL;
+      p = NULL;
+    }
+  }
+
+  return max_len;
 }
 
 int main() {
+  char* s = "abcabcbb";
+  printf("len = %d\n", lengthOfLongestSubstring(s));
 
-  printf("Hello World!!");
+  s = "bbb";
+  printf("len = %d\n", lengthOfLongestSubstring(s));
+
+  s = "pwwkew";
+  printf("len = %d\n", lengthOfLongestSubstring(s));
+
+   s = "w";
+  printf("len = %d\n", lengthOfLongestSubstring(s));
+
+  s = "ww";
+  printf("len = %d\n", lengthOfLongestSubstring(s));
+
+  s = "aab";
+  printf("len = %d\n", lengthOfLongestSubstring(s));
+
+  return 0;
 }
--- 4.Median of Two Sorted Arrays/main.c
+++ 4.Median of Two Sorted Arrays/main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-double findMedianSortedArrays(int* nums1, int* nums2, int nums2Size) {
+bool is_odd(int num) {
+  if (num % 2 == 0) return true;
+  return false;
+}
 
+int find_min_
+
+double find_odd_median_num(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+
+}
+
+double find_even_median_num(int* nums1, int nums1Size, int* nums2, int nums2Size) {}
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+  if (true == is_odd(nums1Size + nums2Size)) {
+
+    return find_odd_median_num(nums1, nums1Size, nums2, nums2Size);
+  } else {
+
+    return find_even_median_num(nums1, nums1Size, nums2, nums2Size);
+  }
 }
 
 int main() {
